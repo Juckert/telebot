3840b3376620249a95a8bfb611c2162ec2ba0faa
Juckert <130921125+Juckert@users.noreply.github.com>
2023-12-18 13:44:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,83 +11,145 @@ schedule_dict = {}
 
 
 class Schedule:
+    """
+    Сохраняет атрибуты для промпта
+
+    Args:
+        subject (str): Предметы
+    """
     def __init__(self, subject):
         self.subject = subject
-        self.count = None
-        self.time = None
-        self.extra = None
+        self.amount = str()
+        self.time = str()
+        self.extra = str()
 
 
 @bot.message_handler(commands=['start'])
 def greetings(message):
+    """
+    Запуск бота
+
+    Args:
+        message (str): Сообщение пользователя
+
+    Returns:
+        Сообщение (str): "Добро пожаловать" и клавиатуру
+    """
     markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     button_schedule = InlineKeyboardButton('Создать расписание', callback_data='Создать расписание')
     button_recommendations = InlineKeyboardButton('Получить рекомендации', callback_data='Получить рекомендации')
     button_questions = InlineKeyboardButton('Задать вопрос', callback_data='Задать вопрос')
-    button_reminder = InlineKeyboardButton('Установить/отключить напоминания', callback_data='Установить/отключить напоминания')
+    button_reminder = InlineKeyboardButton('Установить напоминания', callback_data='Установить напоминания')
     button_ChatGPT = InlineKeyboardButton('Свободная генерация текста', callback_data='ChatGPT')
     markup.add(button_schedule, button_recommendations, button_questions, button_reminder, button_ChatGPT)
     bot.send_message(chat_id=message.chat.id, text='Добро пожаловать', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: message.text == "Создать расписание")
-def schedule_step(message):
+def schedule_choice(message):
+    """
+    Предоставляет запись предметов для расписания
+
+    Args:
+        message (str): "Создать расписание"
+
+    Returns:
+        Направляет к функции "subject_step"
+    """
     msg = bot.send_message(message.chat.id, 'Какие у вас есть предметы?')
-    bot.register_next_step_handler(msg, subject_step)
+    bot.register_next_step_handler(msg, subject_choice)
 
 
-def subject_step(message):
+def subject_choice(message):
+    """
+    Предоставляет выбор количества предметов
+
+    Args:
+        message (str): Предметы пользователя
+
+    Returns:
+        Клавиатура InlineKeyboardMarkup с выбором количества предметов
+        Направляет к функции "count_step"
+    """
     chat_id = message.chat.id
     subject = message.text
     schedule = Schedule(subject)
     schedule_dict[chat_id] = schedule  
-    keyboard_count = InlineKeyboardMarkup(row_width=1)
-    batton_count_1 = InlineKeyboardButton(text='1', callback_data='1')
-    batton_count_2 = InlineKeyboardButton(text='2', callback_data='2')
-    batton_count_3 = InlineKeyboardButton(text='3', callback_data='3')
-    batton_count_4 = InlineKeyboardButton(text='4', callback_data='4')    
-    keyboard_count.add(batton_count_1, batton_count_2, batton_count_3, batton_count_4,)
-    bot.send_message(chat_id=chat_id, text='Сколько предметов в день вам комофртно изучать?', reply_markup=keyboard_count)
+    keyboard_amount = InlineKeyboardMarkup(row_width=1)
+    button_count_1 = InlineKeyboardButton(text='1', callback_data='1')
+    button_count_2 = InlineKeyboardButton(text='2', callback_data='2')
+    button_count_3 = InlineKeyboardButton(text='3', callback_data='3')
+    button_count_4 = InlineKeyboardButton(text='4', callback_data='4')
+    keyboard_amount.add(button_count_1, button_count_2, button_count_3, button_count_4,)
+    bot.send_message(chat_id=chat_id, text='Сколько предметов в день вам комфортно изучать?', reply_markup=keyboard_amount)
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data in ['1', '2', '3', '4']) 
-def count_step(callback):
+def amount_choice(callback):
+    """
+    Предоставляет выбор времени для начала обучения
+
+    Args:
+        callback (str): Количество предметов
+
+    Returns:
+        Клавиатура InlineKeyboardMarkup с выбором времени учебы
+        Направляет к функции "time_step"
+    """
     chat_id = callback.from_user.id
-    count = callback.data
+    amount = callback.data
     schedule = schedule_dict[chat_id]
-    schedule.count = count
+    schedule.amount = amount
     keyboard_time = InlineKeyboardMarkup(row_width=1)
-    batton_time_1 = InlineKeyboardButton(text='8:30', callback_data='8:30')
-    batton_time_2 = InlineKeyboardButton(text='14:15', callback_data='14:15')
-    batton_time_3 = InlineKeyboardButton(text='16:00', callback_data='16:00')
-    keyboard_time.add(batton_time_1, batton_time_2, batton_time_3)
+    button_time_1 = InlineKeyboardButton(text='8:30', callback_data='8:30')
+    button_time_2 = InlineKeyboardButton(text='14:15', callback_data='14:15')
+    button_time_3 = InlineKeyboardButton(text='16:00', callback_data='16:00')
+    keyboard_time.add(button_time_1, button_time_2, button_time_3)
     bot.send_message(chat_id=chat_id, text='С какого времени будет начинаться учеба?', reply_markup=keyboard_time)
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data in ['8:30', '14:15', '16:00'])
-def time_step(callback):
+def time_choice(callback):
+    """
+    Предоставляет возможность дополнить информацию для расписания
+
+    Args:
+        callback (str): Время выбора учебы
+
+    Returns:
+        Направляет к функции "extra_step"
+    """
     chat_id = callback.from_user.id
     time = callback.data
     schedule = schedule_dict[chat_id]
     schedule.time = time
-    msg = bot.send_message(chat_id=chat_id, text='Расскажите подробнее что вам нужно еще учесть при создании расписания')
-    bot.register_next_step_handler(msg, extra_step)
+    msg = bot.send_message(chat_id=chat_id, text='Расскажите подробнее что вам нужно учесть при создании расписания')
+    bot.register_next_step_handler(msg, extra_choice)
 
 
-def extra_step(message):
+def extra_choice(message):
+    """
+    Подставляет атрибуты класса в промпт
+
+    Args:
+        message (str): Уточнения по расписанию
+
+    Returns:
+        Расписание
+    """
     chat_id = message.chat.id
     extra = message.text
     schedule = schedule_dict[chat_id]
     schedule.extra = extra
     result = gpt4_free(textwrap.dedent(f'''\
-                            Помоги мне сделать расписание для студента. В расписании у меня есть {schedule.subject}. 
-                            Очень важно чтобы количество предметов в день в расписании не должно превышать {schedule.count}. 
-                            Учебный день должен начинаться с {schedule.time}. Перерыв между предметами должен быть 15 минут. 
-                            Один предмет идет полтора часа. {schedule.extra}. 
-                            Предметы в расписании не должны повторяться. 
-                            Мне нужно только расписание без рекомендаций.
-                            Расписание должно быть составлено на русском языке.
-                            '''))
+                    Помоги мне сделать расписание для студента. В расписании у меня есть {schedule.subject}. 
+                    Очень важно чтобы количество предметов в день в расписании не должно превышать {schedule.amount}. 
+                    Учебный день должен начинаться с {schedule.time}. Перерыв между предметами должен быть 15 минут. 
+                    Один предмет идет полтора часа. {schedule.extra}. 
+                    Предметы в расписании не должны повторяться. 
+                    Мне нужно только расписание без рекомендаций.
+                    Расписание должно быть составлено на русском языке.
+                    '''))
     bot.send_message(message.chat.id, result)
 
 
